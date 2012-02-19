af36e7442023cc8e924ec3d8950265799434178a
Denis Peplin <denis.peplin@gmail.com>
2012-02-19 21:50:22 +0400

    Fix tests

--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -25,8 +25,8 @@ describe "products/index" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "9.99".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => nil.to_s, :count => 4
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => nil.to_s, :count => 4
   end
 end
