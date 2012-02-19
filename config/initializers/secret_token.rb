c80a08fdd39557710dd310c5007d5c404d5ac240
Denis Peplin <denis.peplin@gmail.com>
2012-02-19 21:27:46 +0400

    rails new rails-store (3.2.1)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsStore::Application.config.secret_token = '4076416c98f02cc60c7aa5c95cc2372d94d41ebbb808362d3fe8ab8de8264c7398d0b2d80d4befb6bd13c7c3e233bd5f56ffa77ff4ff4abecbf4212f583329fa'
