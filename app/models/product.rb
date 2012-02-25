86222198a2b61174ca0784e08991ef82bc45432f
Denis Peplin <denis.peplin@gmail.com>
2012-02-25 19:06:07 +0400

    Validates presence of price

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,6 @@ class Product < ActiveRecord::Base
   delegate :name, to: :category, prefix: true
   delegate :name, to: :brand, prefix: true
 
-  validates_presence_of :name
+  validates_presence_of :name, :price
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
