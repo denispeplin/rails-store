277ac84c006f185a1c0aeb8b2a53d4e9df470213
Denis Peplin <denis.peplin@gmail.com>
2012-03-09 17:43:52 +0400

    Add category and brand to presence verification

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,6 @@ class Product < ActiveRecord::Base
   delegate :name, to: :category, prefix: true
   delegate :name, to: :brand, prefix: true
 
-  validates_presence_of :name, :price
+  validates_presence_of :name, :price, :brand_id, :category_id
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 end
