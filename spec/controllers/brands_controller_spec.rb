43ef58dd9d2492d012b7b7b9045df8749d1b05d7
Denis Peplin <denis.peplin@gmail.com>
2012-07-21 08:03:58 +0400

    Unbreak controllers tests

--- spec/controllers/brands_controller_spec.rb
+++ spec/controllers/brands_controller_spec.rb
@@ -24,7 +24,7 @@ describe BrandsController do
   # Brand. As you add validations to Brand, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {}
+    {name: 'MyBrand'}
   end
   
   # This should return the minimal set of values that should be in the session
