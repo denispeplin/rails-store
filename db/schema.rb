f330cd6d864a3d83e51b3a9f182c4c7bca111777
Denis Peplin <denis.peplin@gmail.com>
2012-12-19 21:33:25 +0400

    Add photo to Product.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120224194905) do
+ActiveRecord::Schema.define(:version => 20121219172905) do
 
   create_table "brands", :force => true do |t|
     t.string   "name"
@@ -35,8 +35,12 @@ ActiveRecord::Schema.define(:version => 20120224194905) do
     t.decimal  "price"
     t.integer  "category_id"
     t.integer  "brand_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   add_index "products", ["brand_id"], :name => "index_products_on_brand_id"
