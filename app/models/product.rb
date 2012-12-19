67a8b8132662f1436951a04d0703f5d2e528b7c8
Denis Peplin <denis.peplin@gmail.com>
2012-12-19 22:02:20 +0400

    Add second photo.

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,6 +8,7 @@ class Product < ActiveRecord::Base
   validates_presence_of :name, :price, :brand_id, :category_id
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
 
-  attr_accessible :photo, :name, :price, :category_id, :brand_id
+  attr_accessible :photo, :photo2, :name, :price, :category_id, :brand_id
   has_attached_file :photo
+  has_attached_file :photo2
 end
